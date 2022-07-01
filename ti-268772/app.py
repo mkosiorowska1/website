3b5b800fafc2144540eae58473e4f50395bd3b16
mkosiorowska1 <108525753+mkosiorowska1@users.noreply.github.com>
2022-07-01 17:19:28 +0200

    Add files via upload

--- ti-268772/app.py
+++ ti-268772/app.py
@@ -5,10 +5,23 @@ app.secret_key="a"
 
 @app.route("/")
 def index():
-    flash("What's your name?")
     return render_template("index.html")
 
-@app.route("/greet",methods=["POST","GET"])
-def greet():
-    flash("Hi " + str(request.form['name_input']) + ", great to see you")
+@app.route("/glowna")
+def glowna():
+  
     return render_template("index.html")
+
+
+@app.route("/funkcja")
+def funkcja():
+  
+    return render_template("funkcja.html")
+
+@app.route("/autorzy")
+def autorzy():
+
+    return render_template("autorzy.html")   
+
+
+
